99e66b1c268d55ea6c0771e1eb6003d4744154ca
Jordan Hatch <jordan@jordanh.net>
2018-07-25 15:34:52 +1000

    Add new SimpleForm input and wrapper for arrays of text fields

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -414,14 +414,15 @@ SimpleForm.setup do |config|
   # Custom wrappers for input types. This should be a hash containing an input
   # type as key and the wrapper that will be used for all inputs with specified type.
   config.wrapper_mappings = {
-    boolean:       :vertical_boolean,
-    check_boxes:   :vertical_collection,
-    date:          :vertical_multi_select,
-    datetime:      :vertical_multi_select,
-    file:          :vertical_file,
-    radio_buttons: :vertical_collection,
-    range:         :vertical_range,
-    time:          :vertical_multi_select
+    boolean:          :vertical_boolean,
+    check_boxes:      :vertical_collection,
+    date:             :vertical_multi_select,
+    datetime:         :vertical_multi_select,
+    file:             :vertical_file,
+    radio_buttons:    :vertical_collection,
+    range:            :vertical_range,
+    time:             :vertical_multi_select,
+    text_field_array: :text_field_array,
   }
 
   # enable custom form wrappers
