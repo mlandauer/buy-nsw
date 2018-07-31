6ec0a907fc7123cbabd95ba3551989bae28d33ae
Jordan Hatch <jordan@jordanh.net>
2018-07-31 13:53:21 +1000

    Migrate addresses to a JSON field on seller versions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180725020310) do
+ActiveRecord::Schema.define(version: 20180731034736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -346,6 +346,7 @@ ActiveRecord::Schema.define(version: 20180725020310) do
     t.text "awards", default: [], array: true
     t.text "accreditations", default: [], array: true
     t.text "engagements", default: [], array: true
+    t.jsonb "addresses", default: []
   end
 
   create_table "sellers", force: :cascade do |t|
