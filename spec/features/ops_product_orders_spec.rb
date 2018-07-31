ec5eb871076b069a58137b1b5b6a831bbe80043f
Jordan Hatch <jordan@jordanh.net>
2018-07-31 11:41:45 +1000

    Fix display of seller name in the ops orders listing

---
+++ spec/features/ops_product_orders_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Reviewing product orders', type: :feature, js: true do
+
+  describe 'as an admin user', user: :admin_user do
+    it 'can view the order list' do
+      orders = create_list(:product_order, 3)
+
+      visit '/ops'
+      click_on 'Product Orders'
+
+      expect(page).to have_content('3 product orders')
+
+      within '.record-list' do
+        expect(page).to have_content(orders.first.buyer.name)
+      end
+    end
+  end
+
+end
