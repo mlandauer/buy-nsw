ccad1b7e08ec1d86de8c3b29fe531d10b2ceb3ca
Jordan Hatch <jordan@jordanh.net>
2018-08-06 12:20:42 +1000

    Only use Bootsnap in development and test

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup'
+
+if Rails.env.development? || Rails.env.test?
+  require 'bootsnap/setup'
+end
