0c661544abd60cb93f85d59c78972e3733a0500e
Jordan Hatch <jordan@jordanh.net>
2018-08-07 17:33:33 +1000

    Simplify test factories for buyers and applications

--- spec/factories/buyer_applications.rb
+++ spec/factories/buyer_applications.rb
@@ -1,48 +1,77 @@
 FactoryBot.define do
   factory :buyer_application do
     association :buyer
-    state 'created'
-    application_body 'Text'
+    association :user
 
-    trait :with_completed_buyer_profile do
-      association :buyer, factory: :inactive_completed_buyer
+    state 'created'
+
+    trait :completed do
+      name 'Sarah'
+      organisation 'Organisation Name'
+      application_body 'Text'
+      employment_status 'employee'
+    end
+
+    trait :contractor do
+      employment_status 'contractor'
     end
 
     trait :manager_approval do
+      contractor
+
       manager_name 'Manager Manager'
       manager_email 'manager@example.org'
+    end
+
+    trait :with_manager_approval_token do
       sequence(:manager_approval_token) {|n| n }
     end
 
     trait :created do
       state 'created'
     end
+
     trait :awaiting_manager_approval do
-      state 'awaiting_manager_approval'
+      completed
       manager_approval
+      with_manager_approval_token
+
+      state 'awaiting_manager_approval'
     end
+
     trait :awaiting_assignment do
+      completed
+
       state 'awaiting_assignment'
     end
+    
     trait :ready_for_review do
+      completed
+
       state 'ready_for_review'
     end
 
     trait :approved do
+      completed
+
       state 'approved'
       decision_body 'Welcome'
     end
 
     trait :rejected do
+      completed
+
       state 'rejected'
       decision_body 'Sorry, no.'
     end
 
     factory :created_buyer_application, traits: [:created]
     factory :created_manager_approval_buyer_application, traits: [:created, :manager_approval]
-    factory :awaiting_manager_approval_buyer_application, traits: [:awaiting_manager_approval, :with_completed_buyer_profile]
-    factory :awaiting_assignment_buyer_application, traits: [:awaiting_assignment, :with_completed_buyer_profile]
-    factory :ready_for_review_buyer_application, traits: [:ready_for_review, :with_completed_buyer_profile]
+    factory :completed_buyer_application, traits: [:created, :completed]
+    factory :completed_manager_approval_buyer_application, traits: [:created, :completed, :manager_approval]
+    factory :awaiting_manager_approval_buyer_application, traits: [:awaiting_manager_approval]
+    factory :awaiting_assignment_buyer_application, traits: [:awaiting_assignment]
+    factory :ready_for_review_buyer_application, traits: [:ready_for_review]
     factory :approved_buyer_application, traits: [:approved]
     factory :rejected_buyer_application, traits: [:rejected]
   end
--- spec/factories/buyers.rb
+++ spec/factories/buyers.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :buyer do
     association :user
-    name 'Sarah'
+
     trait :inactive do
       state :inactive
     end
@@ -10,19 +10,7 @@ FactoryBot.define do
       state :active
     end
 
-    trait :contractor do
-      employment_status 'contractor'
-    end
-
-    trait :completed do
-      name 'Buyer Buyer'
-      organisation 'Organisation Name'
-      employment_status 'employee'
-    end
-
-    factory :active_buyer, traits: [:active, :completed]
+    factory :active_buyer, traits: [:active]
     factory :inactive_buyer, traits: [:inactive]
-    factory :inactive_completed_buyer, traits: [:inactive, :completed]
-    factory :inactive_completed_contractor_buyer, traits: [:inactive, :completed, :contractor]
   end
 end
