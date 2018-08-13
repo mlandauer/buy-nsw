bb174a65eadc38f591e4dd4716a575287624f440
Jordan Hatch <jordan@jordanh.net>
2018-08-13 11:30:57 +1000

    Refactor the seller recognition form with features and benefits behaviour

--- app/concepts/sellers/seller_version/contract/recognition.rb
+++ app/concepts/sellers/seller_version/contract/recognition.rb
@@ -1,57 +1,22 @@
 module Sellers::SellerVersion::Contract
   class Recognition < Base
-
-    AccreditationPrepopulator = ->(_) {
-      self.accreditations ||= []
-
-      count = 0
-      while (self.accreditations.size < 10 && count < 2)
-        self.accreditations << ''
-        count += 1
-      end
-    }
-
-    AwardPrepopulator = ->(_) {
-      self.awards ||= []
-
-      count = 0
-      while (self.awards.size < 10 && count < 2)
-        self.awards << ''
-        count += 1
-      end
-    }
-
-    EngagementPrepopulator = ->(_) {
-      self.engagements ||= []
-
-      count = 0
-      while (self.engagements.size < 10 && count < 2)
-        self.engagements << ''
-        count += 1
-      end
-    }
+    include Concerns::Contracts::Populators
 
     def accreditations=(array)
-      super(filter_blank_values(array))
+      super(filter_blank_array_values(array))
     end
 
     def awards=(array)
-      super(filter_blank_values(array))
+      super(filter_blank_array_values(array))
     end
 
     def engagements=(array)
-      super(filter_blank_values(array))
+      super(filter_blank_array_values(array))
     end
 
-    def filter_blank_values(array)
-      Array(array).reject{|row|
-        row.gsub(/\s/,'').blank?
-      }
-    end
-
-    collection :accreditations, prepopulator: AccreditationPrepopulator
-    collection :awards, prepopulator: AwardPrepopulator
-    collection :engagements, prepopulator: EngagementPrepopulator
+    collection :accreditations, prepopulator: ->(_){ TextFieldArrayPrepopulator.(self, name: :accreditations, limit: 10) }
+    collection :awards, prepopulator: ->(_){ TextFieldArrayPrepopulator.(self, name: :awards, limit: 10) }
+    collection :engagements, prepopulator: ->(_){ TextFieldArrayPrepopulator.(self, name: :engagements, limit: 10) }
 
     validation :default, inherit: true do
       required(:accreditations).value(max_items?: 10)
