fa83bf8db087cc2c84895f818054b95ba8639570
Jordan Hatch <jordan@jordanh.net>
2018-08-14 16:12:30 +1000

    Allow buyer applicants to go back and change their answers

---
+++ app/helpers/buyers/applications_helper.rb
@@ -0,0 +1,11 @@
+module Buyers::ApplicationsHelper
+
+  def change_link(step)
+    link_to('Change',
+      buyers_application_step_path(application, step),
+      class: 'change-link',
+      title: 'Change this answer',
+    )
+  end
+
+end
