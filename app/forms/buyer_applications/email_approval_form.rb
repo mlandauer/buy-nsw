aefc7624249be2ac17b7635bbc38fe2ffcfc1c68
Jerry Chen <jcdsta@gmail.com>
2018-08-22 16:49:25 +1000

    Fix broken staging
    -Remove the 'buybug' line in the forms/buyer_application/email_approval_form.rb

--- app/forms/buyer_applications/email_approval_form.rb
+++ app/forms/buyer_applications/email_approval_form.rb
@@ -7,15 +7,10 @@ module BuyerApplications
 
     validation :default do
 
-      byebug
       required(:application_body).filled
       required(:cloud_purchase, in_list?: BuyerApplication.cloud_purchase.options).filled
       required(:contactable, in_list?: BuyerApplication.contactable.options).filled
 
-
-      #rule(contactable: [:contactable]) do |contactable|
-        #abn.filled? > abn.abn?
-      #end
     end
   end
 end
