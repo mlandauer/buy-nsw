e986a155ed7cda23417c574658b0df7409635848
Jerry Chen <jcdsta@gmail.com>
2018-08-23 17:39:31 +1000

    Revert previous 2 commits
    This reverts commit 745a7d49fc7b18ee394e071f1dc08f182b2c2f34
             and commit aefc7624249be2ac17b7635bbc38fe2ffcfc1c68.

--- app/forms/buyer_applications/email_approval_form.rb
+++ app/forms/buyer_applications/email_approval_form.rb
@@ -7,10 +7,15 @@ module BuyerApplications
 
     validation :default do
 
+      byebug
       required(:application_body).filled
       required(:cloud_purchase, in_list?: BuyerApplication.cloud_purchase.options).filled
       required(:contactable, in_list?: BuyerApplication.contactable.options).filled
 
+
+      #rule(contactable: [:contactable]) do |contactable|
+        #abn.filled? > abn.abn?
+      #end
     end
   end
 end
