1373a64d250c798e2e1b6ced4ee603fc9481b60e
Brendan Roy <br3ndanr@gmail.com>
2018-09-03 16:24:04 +1000

    add and configure user session pretender
    add pretender gem
    add admin/users controller and views
    update application layout and styles to correctly reflect
     when a user session is being pretended

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,16 @@
+class Admin::UsersController < Admin::BaseController
+  def index
+    @users = User.order(:id)
+  end
+
+  def impersonate
+    user = User.find(params[:id])
+    impersonate_user(user)
+    redirect_to root_path
+  end
+
+  def stop_impersonating
+    stop_impersonating_user
+    redirect_to root_path
+  end
+end
