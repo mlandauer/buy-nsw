2c60731b9f86c64d3dcff7af91176e0d2776bbbf
Brendan Roy <br3ndanr@gmail.com>
2018-09-04 13:12:02 +1000

    add SLOW option for capybara

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,6 +1,19 @@
 require "selenium/webdriver"
 
 Capybara.register_driver :chrome do |app|
+  if ENV["SLOW"].present?
+    module ::Selenium::WebDriver::Remote
+      class Bridge
+        alias old_execute execute
+
+        def execute(*args)
+          sleep(0.2)
+          old_execute(*args)
+        end
+      end
+    end
+  end
+
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
@@ -14,7 +27,7 @@ Capybara.register_driver :headless_chrome do |app|
     desired_capabilities: capabilities
 end
 
-Capybara.javascript_driver = :headless_chrome
+Capybara.javascript_driver = ENV["SLOW"].present? ? :chrome : :headless_chrome
 
 # Enable automatic_label_click to support our custom checkbox and radio button
 # implementations. When this option is disabled, our JS tests fail as the driver
