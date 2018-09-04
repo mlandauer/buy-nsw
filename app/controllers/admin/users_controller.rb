601ef75bc42d431ee918506a1ec7e63f4b8e9d02
Brendan Roy <br3ndanr@gmail.com>
2018-09-04 11:05:49 +1000

    add ops panel for users
    add relevant views and search controllers for new ops page
    update stylesheets with new styles for ops panel
    update user model to enable search functionality and ordering
    restrict user session pretending to non-admin users only

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,16 +1,31 @@
 class Admin::UsersController < Admin::BaseController
+  before_action :authenticate_admin!
+  skip_before_action :authenticate_admin!, only: [:stop_impersonating]
+
   def index
-    @users = User.order(:id)
   end
 
   def impersonate
-    user = User.find(params[:id])
+    user = User.without_role("admin").find(params[:id])
     impersonate_user(user)
     redirect_to root_path
   end
 
   def stop_impersonating
     stop_impersonating_user
-    redirect_to root_path
+    redirect_to admin_users_path
   end
+
+  def search
+    @search ||= Search::Admin::User.new(
+      selected_filters: params,
+      default_values: {
+      },
+      page: params.fetch(:page, 1),
+      per_page: 50,
+    )
+  end
+  helper_method :search
+
+
 end
