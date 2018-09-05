50520c58aeeae6b2472801dba6e239623f9036bb
Jerry Chen <jcdsta@gmail.com>
2018-09-05 14:19:54 +1000

    Soft-delete user (#349)
    * Soft-delete user
    
    this uses a gem called 'discard' https://github.com/jhawthorn/discard#why-not-paranoia-or-acts_as_paranoid
    changes also done in the following:
    - Gemfile
    - models
    - db schema
    - unit testing
    
    * remove the unneeded 'if' in view _events.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180820050128) do
+ActiveRecord::Schema.define(version: 20180903055122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -329,7 +329,9 @@ ActiveRecord::Schema.define(version: 20180820050128) do
     t.datetime "updated_at", null: false
     t.text "roles", default: [], array: true
     t.integer "seller_id"
+    t.datetime "discarded_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["discarded_at"], name: "index_users_on_discarded_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
