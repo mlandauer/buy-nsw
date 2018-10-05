a42b99eba30ece0aae33bd685b7591d370ab0d44
Brendan Roy <br3ndanr@gmail.com>
2018-10-05 10:35:45 +1000

    fix extra output from rubocop spec

--- spec/lint/rubocop_spec.rb
+++ spec/lint/rubocop_spec.rb
@@ -2,13 +2,9 @@
 # https://gist.github.com/djburdick/5104d15f612c15dde65f
 require "spec_helper"
 
-RSpec.describe "Check that the files we have changed pass lint checking" do
+RSpec.describe "Check the current codebase can pass lint checks" do
   before do
-    current_sha = `git rev-parse --verify HEAD`.strip!
-    files = `git diff master #{current_sha} --name-only | grep .rb`
-    files.tr!("\n", " ")
-    @report = `rubocop #{files}`
-    puts "Report: #{@report}"
+    @report = `rubocop`
   end
 
   it { expect(@report.match("Offenses")).to be_nil, "Rubocop offenses found.\n#{@report}" }
